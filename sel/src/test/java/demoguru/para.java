7f712a06a016060de6df78a8e4c6f4a3d6b816a9
harshkacha1103 <99968991+harshkacha1103@users.noreply.github.com>
2024-09-29 19:27:44 +0530

    Commit
    Commit

--- sel/src/test/java/demoguru/para.java
+++ sel/src/test/java/demoguru/para.java
@@ -3,16 +3,19 @@ package demoguru;
 import java.io.File;
 import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class para {
@@ -22,15 +25,15 @@ public class para {
 	{
          String[][] data=null;
 		
-		String filepath="E:\\\\LoginAssignment.xlsx";
+		String filepath="D:\\\\LoginAssignment.xlsx";
 		
-         File file = new File(filepath);
+		File file = new File(filepath);
 		
 		XSSFWorkbook workbook=new XSSFWorkbook(file);
 		
 	    Sheet sheet=workbook.getSheet("Sheet1");
-	    
-	    int nrows = sheet.getPhysicalNumberOfRows();
+		
+		int nrows = sheet.getPhysicalNumberOfRows();
 		System.out.println("Number of rows: "+nrows);
 		
 		data=new String[nrows][];
@@ -53,7 +56,7 @@ public class para {
 		return data;
 	}
 	
-     WebDriver driver = null;
+	WebDriver driver = null;
 	
 	@Test
 	public void test() throws IOException, InterruptedException, InvalidFormatException
@@ -76,24 +79,24 @@ public class para {
 			Thread.sleep(2000);
 			
 			
-			Alert alert = driver.switchTo().alert();
-			System.out.println(alert.getText());
-			Thread.sleep(2000);
-			alert.accept();
-			/*
-			 * if(driver.getCurrentUrl().equals("")) {
-			 * System.out.println("Your Test HasBeen Passed..."); } else {
-			 * System.out.println("Your Test HasbeenFailed"); File
-			 * src=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE); File dest=new
-			 * File("D:\\img\\1.png"); FileUtils.copyFile(src, dest); Thread.sleep(2000);
-			 * Assert.fail("Incorrect username or password..."); }
-			 */
+			if(driver.getCurrentUrl().equals("https://www.demo.guru99.com/V41wwu"))
+		{
+			System.out.println("Your Test HasBeen Passed...");
+		}
+			else
+			{
+				System.out.println("Your Test HasbeenFailed");
+				File src=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+				File dest=new File("D:\\img\\32.png");
+				FileUtils.copyFile(src, dest);
+				Thread.sleep(2000);
+	
+			}
 			Thread.sleep(2000);
 			System.out.println("Bye Bye...");
-			driver.close();
+			
 		}
 	
 	}
-	
 
 }
